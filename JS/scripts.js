716fde2017aba8b62ab4e07fc2933d716392fefa
Metidji Sid Ahmed <is_metidji@esi.dz>
2021-02-25 04:25:34 +0100

    finishing the design
    the content didnt put yet !

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,4 +1,59 @@
-$(function () {
+var body = document.body,
+    html = document.documentElement;
+
+var maxY = Math.max(body.scrollHeight, body.offsetHeight,
+    html.clientHeight, html.scrollHeight, html.offsetHeight) - 250;
+let maxX = Math.max(
+    document.body.scrollWidth,
+    document.documentElement.scrollWidth,
+    document.body.offsetWidth,
+    document.documentElement.offsetWidth,
+    document.documentElement.clientWidth
+) - 250;
+
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+let x1 = document.createElement('img');
+x1.setAttribute("src", "img/XO/x1.png");
+let x2 = document.createElement('img');
+x2.setAttribute("src", "img/XO/x2.png");
+let x3 = document.createElement('img');
+x3.setAttribute("src", "img/XO/x3.png");
+let o1 = document.createElement('img');
+o1.setAttribute("src", "img/XO/o1.png");
+let o2 = document.createElement('img');
+o2.setAttribute("src", "img/XO/o2.png");
+let arr = [x1, x2, x3, o1, o2];
+for (let cpt = 1; cpt != 101; cpt++) {
+    let X = getRndInteger(0, maxX);
+    let Y = getRndInteger(0, maxY);
+    let indice = getRndInteger(0, 4);
+    let cloned = arr[indice].cloneNode(true);
+    cloned.classList.add("x");
+    cloned.setAttribute("style", `top:${Y}px ; left :${X}px`);
+    document.body.appendChild(cloned);
+}
+// console.log(document.querySelector('body'));
+// let x1 = document.createElement('img');
+// x1.setAttribute("src", "img/XO/o1.png");
+// let Y = 0;
+// let X = 0;
+// while (Y < maxY) {
+//     X = 0;
+//     while (X < maxX) {
+//         let cloned = x1.cloneNode(true);
+//         cloned.setAttribute("style", `top:${Y}px ; left :${X}px`);
+//         cloned.classList.add("o");
+//         document.body.appendChild(cloned);
+//         X += 50;
+//     }
+//     Y += 50;
+// }
+
+
+$(function() {
     // Slides
     $('.slick-about').slick({
         slidesToShow: 1,
@@ -12,7 +67,7 @@ $(function () {
     feather.replace();
 
     //page scroll
-    $('a.page-scroll').bind('click', function (event) {
+    $('a.page-scroll').bind('click', function(event) {
         var $anchor = $(this);
         $('html, body').stop().animate({
             scrollTop: $($anchor.attr('href')).offset().top - 20
@@ -23,7 +78,7 @@ $(function () {
 
     //toggle scroll menu
     var scrollTop = 0;
-    $(window).scroll(function () {
+    $(window).scroll(function() {
         var scroll = $(window).scrollTop();
         //adjust menu background
         if (scroll > 80) {
@@ -49,17 +104,17 @@ $(function () {
     });
 
     // scroll top top
-    $('.scroll-top').click(function () {
+    $('.scroll-top').click(function() {
         $('html, body').stop().animate({
             scrollTop: 0
         }, 1000);
     });
 
     /**Theme switcher - DEMO PURPOSE ONLY */
-    $('.switcher-trigger').click(function () {
+    $('.switcher-trigger').click(function() {
         $('.switcher-wrap').toggleClass('active');
     });
-    $('.color-switcher ul li').click(function () {
+    $('.color-switcher ul li').click(function() {
         var color = $(this).attr('data-color');
         $('#theme-color').attr("href", "css/" + color + ".css");
         $('.color-switcher ul li').removeClass('active');
